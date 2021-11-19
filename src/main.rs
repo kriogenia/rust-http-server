c00e15b14256532bb564c605fe297aa3b8d3f904
Kriogenia <47500377+kriogenia@users.noreply.github.com>
2021-11-19 14:04:18 +0100

    Method enum

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,44 @@
 fn main() {
-    let string = String::from("127.0.0.1:8080");
-    let server = Server::new(string);
-    server.run();
+	let server = Server::new("127.0.0.1:8080".to_string());
+	server.run();
 }
 
 struct Server {
 
-    address: String
+	address: String
 
 }
 
 impl Server {
 
-    fn new(address: String) -> Self {
-        Self {
-            address
-        }
-    }
+	fn new(address: String) -> Self {
+		Self {
+			address
+		}
+	}
 
-    fn run(self) {
-        println!("{}",self.address);
-    }
+	fn run(self) {
+		println!("Listening on {}",self.address);
+	}
 
+}
+
+struct Request {
+
+	path: String,
+	query_string: String,
+	method: Method
+
+}
+
+enum Method {
+	GET,
+	DELETE,
+	POST,
+	PUT,
+	HEAD,
+	CONNECT,
+	OPTIONS,
+	TRACE,
+	PATCH
 }
