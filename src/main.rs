840fecaf4aa20c9ffaec35b0bcb6de4ef9c42f4b
Kriogenia <47500377+kriogenia@users.noreply.github.com>
2021-11-19 13:36:56 +0100

    Server base

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
 fn main() {
-    println!("Hello, world!");
+    let string = String::from("127.0.0.1:8080");
+    let server = Server::new(string);
+    server.run();
+}
+
+struct Server {
+
+    address: String
+
+}
+
+impl Server {
+
+    fn new(address: String) -> Self {
+        Self {
+            address
+        }
+    }
+
+    fn run(self) {
+        println!("{}",self.address);
+    }
+
 }
