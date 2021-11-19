7d893f773c51b3feeb7e2dd9942a557ff3f20c75
Kriogenia <47500377+kriogenia@users.noreply.github.com>
2021-11-19 15:29:37 +0100

    Log incoming requests

--- src/server.rs
+++ src/server.rs
@@ -1,4 +1,7 @@
+use std::io::Read;
 use std::net::TcpListener;
+
+const BUFFER_SIZE: usize = 1024;
 
 pub struct Server {
 	address: String
@@ -19,8 +22,16 @@ impl Server {
 
 		loop {
 			match listener.accept() {
-				Ok((_stream, address)) => {
-					println!("Connection received from {}", address);
+				Ok((mut stream, address)) => {
+					println!("\nConnection received from {}", address);
+
+					let mut buffer = [0; BUFFER_SIZE];
+					match stream.read(&mut buffer) {
+						Ok(_) => {
+							println!("Received a request: {}", String::from_utf8_lossy(&buffer));
+						},
+						Err(e) => println!("Fail reading from connection: {}", e)
+					}
 				},
 				Err(e) => continue
 			}
