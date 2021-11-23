69d3dc68e22642b481ede29e9db67ce9c6462d57
Kriogenia <47500377+kriogenia@users.noreply.github.com>
2021-11-23 19:16:22 +0100

    Query and path readings

--- src/http/request/request.rs
+++ src/http/request/request.rs
@@ -5,7 +5,7 @@ use std::str;
 
 pub struct Request {
 	path: String,
-	query_string: Option<String>,
+	query: Option<String>,
 	method: Method,
 }
 
@@ -16,27 +16,37 @@ impl TryFrom<&[u8]> for Request {
 		let request = str::from_utf8(value)?;
 
 		let (method, request) = get_next_word(request).ok_or(ParsingError::InvalidRequest)?;
-		let (path, request) = get_next_word(request).ok_or(ParsingError::InvalidRequest)?;
+		let (reference, request) = get_next_word(request).ok_or(ParsingError::InvalidRequest)?;
 		let (protocol, _) = get_next_word(request).ok_or(ParsingError::InvalidRequest)?;
+		// ignoring the rest of the request for now
 
 		if protocol != "HTTP/1.1" {
 			return Err(ParsingError::InvalidProtocol);
 		}
 
 		let method: Method = method.parse()?;
+		let (path, query) = readReference(reference);
 
 		println!("Path: {}", path);
+		if query.is_some() { println!("Query: {}", query.unwrap()); }
 		println!("Protocol: {}", protocol);
 
-		unimplemented!();
+		!unimplemented!()
 	}
 }
 
 fn get_next_word(request: &str) -> Option<(&str, &str)> {
 	for (i, c) in request.chars().enumerate() {
-		if c == ' ' || c == '\r' || c == '\n'  {
+		if c.is_whitespace()  {
 			return Some((&request[..i], &request[i + 1..]));
 		}
 	}
 	return None;
+}
+
+fn readReference(reference: &str) -> (&str, Option<&str>) {
+	match reference.find('?') {
+		Some(i) => (&reference[..i], Some(&reference[i + 1..])),
+		None => (&reference, None)
+	}
 }
